aaba840a19eaaea53544abaed95df7c60c3c156b
Aleksei Zvonarev <aleksei.zvonarev@autodesk.com>
2025-09-16 13:13:15 +0200

    Remove multiple outdated HTML, CSS, JavaScript, image, and video files from the project to streamline the codebase and enhance maintainability.

--- assets/js/mypersonal.js
+++ assets/js/mypersonal.js
@@ -2,7 +2,7 @@
 
             var password = prompt('Enter the password to download the file:');
             if (password.toLowerCase() == "teacher") {
-        window.open("AZ_files/AZ_CV.pdf")
+        window.open("assets/documents/AZ_CV.pdf")
                 return true;
             } else {
         alert("Incorrect password!! please try again");
